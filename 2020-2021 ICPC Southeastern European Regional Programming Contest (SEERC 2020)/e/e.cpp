ed29a8bd94fe285d5e9201e8d8d7e0e747ef8193
Ghost-lzw <lanzongwei541@gmail.com>
2021-06-26 17:23:54 +0800

    fix seerc 2020 e

--- 2020-2021 ICPC Southeastern European Regional Programming Contest (SEERC 2020)/e/e.cpp
+++ 2020-2021 ICPC Southeastern European Regional Programming Contest (SEERC 2020)/e/e.cpp
@@ -1,53 +1,43 @@
-/*************************************************************************
-    > File Name: e.cpp
-    > Author: ghost_lzw
-    > Mail: lanzongwei@gmail.com 
-    > Created Time: Sat Jun 26 09:28:42 2021
- ************************************************************************/
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, x) for (int i = 0; i < x; i++)
-constexpr int Ma = 1e6, mod = 3;
-using ll=long long;
-int a[Ma];
-int pre[2][3][3];
+typedef long long ll;
 
-#define debug(x) cerr<<#x<<" == " << x<<endl
+const int M = (int)5e5;
 
-int norm(int x) {
-	if (x < 0) x %= mod;
-	x += mod;
-	return x % mod;
+int n, a[M + 5];
+int cnt[3][3];
+
+int cal(int n1, int n2)
+{
+    return (1 * n1 * (n1 - 1) / 2 + 4 * n2 * (n2 - 1) / 2 + 2 * n1 * n2) % 3;
 }
 
-int main() {
-	int n; cin >> n;
-	rep (i, n) cin >> a[i];
-	ll ans = 0;
-	int cur = 0;
-	pre[0][0][a[0] % 3]++;
-	for (int i = 1; i < n; i++) {
-		cur ^= 1;
-		a[i] %= 3;
-		memset(pre[cur], 0, sizeof pre[cur]);
-		pre[cur][0][0] +=
-			pre[cur ^ 1][0][0] +
-			pre[cur ^ 1][1][] +
-			pre[cur ^ 1][2][norm(1 - j - a[i])];
-		pre[cur][1][0] +=
-			pre[cur ^ 1][0][norm(1 - j - a[i])] +
-			pre[cur ^ 1][1][norm(3 - j - a[i])] +
-			pre[cur ^ 1][2][norm(2 - j - a[i])];
-		pre[cur][2][0] +=
-			pre[cur ^ 1][0][norm(2 - j - a[i])] +
-			pre[cur ^ 1][1][norm(1 - j - a[i])] +
-			pre[cur ^ 1][2][norm(3 - j - a[i])];
-		for (int j = 0; j < 3; j++)	
-			ans += pre[cur][0][j];
-	}
-	cout << ans << endl;
+void work()
+{
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i) scanf("%d", &a[i]);
+    int n1 = 0, n2 = 0;
+    ++cnt[n1][n2];
+    ll ans = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        n1 = (n1 + (a[i] % 3 == 1)) % 3;
+        n2 = (n2 + (a[i] % 3 == 2)) % 3;
+        for(int n3 = 0; n3 <= 2; ++n3)
+        {
+            for(int n4 = 0; n4 <= 2; ++n4)
+            {
+                if(cal(n1 - n3, n2 - n4) == 0) ans += cnt[n3][n4];
+            }
+        }
+        ++cnt[n1][n2];
+    }
+    printf("%lld\n", ans);
+}
 
-	return 0;
+int main()
+{
+    work();
+    return 0;
 }
